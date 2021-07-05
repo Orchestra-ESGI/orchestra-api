bf653a83c4123685a051772c3bf655cd73b93a6d
Nassim <nassim.morouche1@gmail.com>
2021-07-05 14:59:33 +0200

    Finished tokens

--- routes/firebase.js
+++ routes/firebase.js
@@ -26,15 +26,33 @@ router.get('/all', verifyHeaders, async (req, res) => {
     }
 });
 
-router.post('/', async (req, res) => {
+router.post('/', verifyHeaders, async (req, res) => {
     try {
         await connectMongoClient();
         const col = client.db("orchestra").collection('fcm');
 
-        const result = await col.find({ token: req.body.token }).toArray();
+        const result = await col.find({ user_id: req.token._id }).toArray();
         if (result.length == 0) {
             await col.insertOne(req.body);
+        } else {
+            await col.updateOne({ user_id: req.token._id }, req.body);
         }
+
+        res.status(200).send({
+            error: null
+        })
+    } catch (err) {
+        res.status(500).send({
+            error: err
+        });
+    }
+});
+
+router.delete('/clear', verifyHeaders, async (req, res) => {
+    try {
+        await connectMongoClient();
+        const col = client.db("orchestra").collection('fcm');
+        await col.deleteMany({});
 
         res.status(200).send({
             error: null
